fa5b592d0177ab6b36a8b43d0bf746ef192a7bfb
herbert <herbert_git@163.com>
2016-12-16 19:22:29 +0800

    m1

---
+++ src/main/java/InterDemo/IntegerTest.java
@@ -0,0 +1,16 @@
+package InterDemo;
+
+/**
+ * Created by maywi_000 on 2016/12/15.
+ */
+public class IntegerTest {
+    public static void main(String[] args) {
+//        System.out.println("hello git!");
+        Integer a = 200;
+        Integer b = 200;
+        System.out.println(a.equals(b));
+        System.out.println(a == b);
+        Integer c = Integer.MAX_VALUE;
+        System.out.println(c);
+    }
+}
